61a0737f93e2fc9274c16a711ab9a63ac0abe776
bhirsch42 <benhirsch42@gmail.com>
2016-10-24 14:51:30 -0400

    Added Build and Part collections

--- client/controllers/register.js
+++ client/controllers/register.js
@@ -1,3 +1,9 @@
+Template.select_build.helpers({
+  builds: () => {
+    return Builds.find({}).fetch()
+  }
+})
+
 // This is not how build selection will eventually work.
 // We'll use reactive variables when the builds are dynamic.
 Template.build.events({
@@ -5,5 +11,17 @@ Template.build.events({
     $target = $(e.target)
     $('.selected').removeClass('selected').addClass('tertiary')
     $target.removeClass('tertiary').addClass('selected');
+  }
+})
+
+Template.current_build.helpers({
+  builds: function() {
+    user = Accounts.user();
+    if (!user.build) {
+      user.build = Builds.findOne({})._id;
+    }
+    Accounts.users.insert(user);
+    console.log(user);
+    return Builds.find(user.build);
   }
 })
